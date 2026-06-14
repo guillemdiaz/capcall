13093b2fba989012e518d96d6ee9051989615c4f
guillemdiaz <gdiazcabanas@gmail.com>
2026-06-14 13:33:11 +0200

    feat: configure admin panel with custom investor fields

--- backend/core/admin.py
+++ backend/core/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from core.models import Fund, Investor, Subscription
+
+admin.site.register(Fund)
+admin.site.register(Subscription)
+
+
+class InvestorAdmin(UserAdmin):
+    # Add User
+    add_fieldsets = (UserAdmin.add_fieldsets or ()) + (
+        ("Investor Profile", {"fields": ("investor_type", "kyc_status", "country")}),
+    )
+
+    # Change User
+    fieldsets = (UserAdmin.fieldsets or ()) + (
+        ("Investor Profile", {"fields": ("investor_type", "kyc_status", "country")}),
+    )
+
+    # Columns in the general list
+    list_display = ["username", "email", "investor_type", "kyc_status", "is_staff"]
+
+
+admin.site.register(Investor, InvestorAdmin)
